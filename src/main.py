b86f0515e7d6333d10bd9767e364fe609a8e6167
Taddy <thaddadavis@gmail.com>
2026-07-28 20:31:45 -0400

    adding to CORS whitelist

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ app.root_path = ""
 jwt_allowed_origins = [
     "https://kalygo.io",
     "https://bolay.kalygo.io",
-    "https://cmdlabs.io",
+    "https://www.cmdlabs.io",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
